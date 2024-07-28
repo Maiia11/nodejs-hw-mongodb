04c4195f5697d9ceddde057f1e4c501de8afd31e
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-07-28 17:46:45 +0300

    final

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from "dotenv";
 
+import { getAllContacts, getAllContactById } from './services/contacts.js';
+
 dotenv.config();
 
 const PORT = Number(process.env.PORT) || 3000;
@@ -18,7 +20,37 @@ export const setupServer = () => {
       target: 'pino-pretty',
     },
   }),
-);
+    );
+
+    app.get("/contacts", async (req, res, next) => {
+        const contacts = await getAllContacts();
+
+        res.status(200).json({
+            status: 200,
+            message: "Successfully found contacts!",
+            data: contacts,
+        });
+
+    });
+
+    app.get("/contacts/:contactId", async (req, res, next) => {
+        const { contactId } = req.params;
+        const contact = await getAllContactById(contactId);
+
+        if (!contact) {
+            res.status(404).json({
+                message: 'Contact not found'
+            });
+            return;
+        }
+
+        res.status(200).json({
+            status: 200,
+            message: `Successfully found contact with id ${contactId}!`,
+            data: contact
+        });
+
+    });
 
     app.use('*', (req, res, next) => {
         res.status(404).json({
