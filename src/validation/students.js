403d77cca05a1067f6eec81bd506a5352cbaa7f5
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-08-13 18:47:39 +0300

    add schems validation body

---
+++ src/validation/students.js
@@ -0,0 +1,37 @@
+import Joi from "joi";
+
+export const createContactSchema = Joi.object({
+    name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+    phoneNumber: Joi.string().min(3).max(20).required().messages({
+        'string.min': 'phoneNumber should have at least {#limit} characters',
+    'string.max': 'phoneNumber should have at most {#limit} characters',
+        'any.required': 'PhoneNumber is required',
+    }),
+    email: Joi.string().min(3).max(20).email(),
+    isFavourite: Joi.boolean(),
+    contactType: Joi.string().valid('work', 'home', 'personal').required().messages({
+        'any.required': 'contactType is required',
+    }),
+
+});
+
+export const patchContactSchema = Joi.object({
+    name: Joi.string().min(3).max(20).messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+  }),
+    phoneNumber: Joi.string().min(3).max(20).messages({
+        'string.min': 'phoneNumber should have at least {#limit} characters',
+    'string.max': 'phoneNumber should have at most {#limit} characters',
+    }),
+    email: Joi.string().min(3).max(20).email(),
+    isFavourite: Joi.boolean(),
+    contactType: Joi.string().valid('work', 'home', 'personal')
+
+});
